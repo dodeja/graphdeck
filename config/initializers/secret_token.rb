864ae7dfa3218413d3e5444967258b39c75b7db3
Hubert Wong <hwong@me.com>
2010-10-20 17:43:42 -0700

    New Rails 3.0.1 project on MySQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Graphdeck::Application.config.secret_token = '3fd832fe43e61aa0db84168be33ac74253e11d6c3d8944a8d1d82ab194ba19388da1ca29971c967d582aad2de1a3afb15c69c893c7f589767583ad024d7c8f6a'
