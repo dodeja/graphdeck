1eaf18ea91bf926d707b87204f3616d52ab0b6fd
Hubert Wong <hwong@me.com>
2010-11-02 13:32:57 -0700

    Fixed up login/logout issues. Fixed bug in namespaces_users join table migration

--- db/migrate/20101102050432_create_namespaces_users_join_table.rb
+++ db/migrate/20101102050432_create_namespaces_users_join_table.rb
@@ -1,6 +1,6 @@
 class CreateNamespacesUsersJoinTable < ActiveRecord::Migration
   def self.up
-    create_table :namespaces_users do |t|
+    create_table :namespaces_users, :id => false do |t|
       t.integer :namespace_id
       t.integer :user_id
       t.integer :authorizer_id
