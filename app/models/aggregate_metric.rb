a52b0d95a1db15a279c1eb61d9580a4d06f8521c
Hubert Wong <hwong@me.com>
2010-12-15 01:05:57 -0800

    Added AggregateMetric.jsonp_api

--- app/models/aggregate_metric.rb
+++ app/models/aggregate_metric.rb
@@ -22,4 +22,30 @@ class AggregateMetric < ActiveRecord::Base
   def self.metric_types
     COUNT..TP100
   end
+  
+  #  AggregateMetric.view_api(:namespace => params[:namespace_id], :name => @name, :type => @type, :from => @from, :to => @to, :duration => @duration)
+  def self.jsonp_api(args)
+    result = {}
+    result[:name] = name = args[:name]
+    result[:namespace] = namespace = args[:namespace]
+    result[:type] = type = args[:type].to_i
+    result[:from] = from = args[:from].to_i
+    result[:to] = to = args[:to].to_i
+    result[:duration] = duration = args[:duration].to_i
+    
+    if name.nil? or namespace.nil? or type.nil? or from.nil? or to.nil? or duration.nil?
+      return result
+    end
+    
+    result[:tsd] = []
+
+    namespace_id = Namespace.find_by_name(namespace).id
+    metrics = self.find_all_by_name(name, :conditions => ['namespace_id = ? and metric_type = ? and timestamp >= ? and timestamp <= ? and duration = ?', namespace_id, type, from, to, duration])
+    metrics.each do |metric,index|
+      result[:tsd] << {:timestamp => metric.timestamp.to_i, :value => metric.value}
+    end
+    
+    result
+  end
+  
 end
